49d4a316a7c9fa33b1a87fe7f18ba3ae85800551
Balazs Kelemen <10376327+prampec@users.noreply.github.com>
2018-06-05 00:13:56 +0200

    Introducing WebRelay example.

--- src/IotWebConf.cpp
+++ src/IotWebConf.cpp
@@ -4,7 +4,7 @@
  *   https://github.com/prampec/IotWebConf 
  *
  * Copyright (C) 2018 Balazs Kelemen <prampec+arduino@gmail.com>
- *c
+ *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
